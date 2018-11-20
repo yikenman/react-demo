7fad20375d80930f9f0e0bf3a24b759a266d034b
chenl <chenlu3@xiao100.com>
2018-11-20 18:04:14 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ interface IChildProps {
   title: string;
   color?: string;
 }
-const Child: React.FunctionComponent<IChildProps> = props => {
+const Child: React.FC<IChildProps> = props => {
     const [counts, updateCounts] = useState(0);
     const timeService = useProvider(TimerService);
     useEffect(() => {
@@ -20,18 +20,31 @@ const Child: React.FunctionComponent<IChildProps> = props => {
     <div className="child-border" style={{ borderColor: props.color || 'black' }}>
       <div>{props.title || ""}</div>
       <div>{counts}</div>
+      <div>{props.children}</div>
     </div>
   );
 };
 
-const Wapper: React.FunctionComponent<{}> = props => {
+const ChildCopy = withProviders<IChildProps>([TimerService])(Child);
+
+const Wapper: React.FC<{}> = props => {
   return <div>{props.children}</div>;
 };
 
 export const App = withProviders([TimerService])(() => {
+  const [title, updateTitle] = useState(`2`);
+
+  const onClick = () => {
+    updateTitle(+title+1+'')
+  }
+
   return (
     <div>
       <Child title="1" />
+      <ChildCopy title={title} >
+        <div>aaa</div>
+      </ChildCopy>
+      <button onClick={onClick}>button</button>
     </div>
   );
 });
--- src/react-diNg/index.tsx
+++ src/react-diNg/index.tsx
@@ -1,24 +1,26 @@
 import {
   Provider,
-  ReflectiveInjector,
-  ResolvedReflectiveProvider
+  ReflectiveInjector
+  // ResolvedReflectiveProvider
 } from "injection-js";
-import React, { createContext, FunctionComponent, useContext } from "react";
+import React, { createContext, useContext, FC, ProviderProps } from "react";
 
 type Constructor<T = any> = new (...args: any[]) => T;
 
 const initProviders = ReflectiveInjector.resolve([]);
 const initInjector = ReflectiveInjector.fromResolvedProviders(initProviders);
 const Context = createContext(initInjector);
-const resolveProviderStore: { [x: string]: ResolvedReflectiveProvider } = {};
+// TODO: save resolved providers for performance-sensitive cases
+// const resolvedProviderStore: { [x: string]: ResolvedReflectiveProvider } = {};
 
 export function withProviders<P>(providers: Provider[] = []) {
-  return (Component: FunctionComponent<P>): FunctionComponent<P> => {
+  return (Component: FC<P>): FC<P> => {
     if (!providers || !providers.length) {
       return Component;
     }
     return (props: P) => {
       const parentInjector = useContext(Context);
+      // use useMemo/useCallback to reduce re-render
       const injector = parentInjector.resolveAndCreateChild(providers);
       return (
         <Context.Provider value={injector}>
